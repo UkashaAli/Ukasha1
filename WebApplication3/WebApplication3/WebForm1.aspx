b3a765e72ab2f9387cc67628a6aa38df60eee5db
probattle <probattle@iba.edu.pk>
2017-03-04 14:50:00 +0500

    this is

---
+++ WebApplication3/WebApplication3/WebForm1.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<%@ Register assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" namespace="System.Web.UI.DataVisualization.Charting" tagprefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table style="border: 1px solid; font-family: Arial;">
+            <tr>
+                <td>
+                    <b>Select chart type</b>
+                </td>
+
+                <td>
+                    <asp:DropDownList ID="DDL1" AutoPostBack="true" runat="server" OnSelectedIndexChanged="DDL1_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td>
+        <asp:Chart ID="Chart2" runat="server">
+            <Titles>
+                <asp:Title Text="Total marks of students">
+                </asp:Title>
+            </Titles>
+            <series>
+                <asp:Series Name="Series1" ChartArea="ChartArea1" ChartType="Pie">
+                </asp:Series>
+            </series>
+            <chartareas>
+                <asp:ChartArea Name="ChartArea1">
+                    <AxisX Title="Students name">
+                    </AxisX>
+                    <AxisY Title="Total marks"></AxisY>
+                </asp:ChartArea>
+            </chartareas>
+        </asp:Chart>
+
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.DataVisualization.Charting;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetChartData();
+                GetChartTypes();
+            }
+        
+
+        }
+        private void GetChartData()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["CS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select Student Name, Total Marks from Students", con);
+                Series series = Chart2.Series["Series1"];
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while(rdr.Read()){
+                    series.Points.AddXY(rdr["Student Name"].ToString(), rdr["Total Marks"]);
+                }
+            }
+        }
+        private void GetChartTypes()
+        {
+            foreach(int chartType in Enum.GetValues(typeof(SeriesChartType)))
+            {
+                ListItem li = new ListItem(Enum.GetName(typeof(SeriesChartType), chartType), chartType.ToString());
+                DDL1.Items.Add(li);
+            }
+        }
+
+        protected void DDL1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetChartData();
+            Chart2.Series["Series1"].ChartType = (SeriesChartType)Enum.Parse(typeof(SeriesChartType), DDL1.SelectedValue);
+        }
+    }
+}
